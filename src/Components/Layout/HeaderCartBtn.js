04056f3504fcef41bd300190c9b1c025977989b8
AlexaNem0 <vikalyeksyeyev@gmail.com>
2022-10-27 21:08:44 -0500

    final commit

--- src/Components/Layout/HeaderCartBtn.js
+++ src/Components/Layout/HeaderCartBtn.js
@@ -1,19 +1,35 @@
 import React from "react";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartIcon from "../Cart/CartIcon";
 import CartContext from "../../store/cart-context";
 import classes from "./HeaderCartBtn.module.css";
 
 const HeaderCartBtn = (props) => {
+  const [btnAnimation, setBtnAnimation] = useState(false);
   const cartCtx = useContext(CartContext);
 
   const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
 
+  const { items } = cartCtx;
+
+  const btnClasses = `${classes.button} ${btnAnimation ? classes.bump : ""}`;
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnAnimation(true);
+    const timer = setTimeout(() => {
+      setBtnAnimation(false);
+    }, 300);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
   return (
     <>
-      <button className={classes.button} onClick={props.onClick}>
+      <button className={btnClasses} onClick={props.onClick}>
         <span className={classes.icon}>
           <CartIcon />
         </span>
